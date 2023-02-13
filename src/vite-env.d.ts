3343129e13ce7b223c2b379d91424499f6a23895
alexders <alexders@users.noreply.github.com>
2023-02-13 09:09:52 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 7f8206202fb65ba27ee8afb204e92e2878e22049

--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-// 识别vue文件
-    declare module '*.vue' {
-        import type { DefineComponent } from 'vue';
-        const vueComponent: DefineComponent<{}, {}, any>;
-        export default vueComponent;
-    
- }
